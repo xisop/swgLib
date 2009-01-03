29063be0023844751a50b5adb92cf4ecd73a6110
Kenneth R. Sewell III <sewellkr3@gmail.com>
2009-01-03 03:46:19 +0000

    Add variables and functions.
    git-svn-id: file:///home/svn/meshLib@20 a422a631-36e5-491f-99c6-f798677b70e4

--- include/meshLib/cell.hpp
+++ include/meshLib/cell.hpp
@@ -4,7 +4,7 @@
  *  \author Kenneth R. Sewell III
 
  meshLib is used for the parsing and exporting .msh models.
- Copyright (C) 2006,2007 Kenneth R. Sewell III
+ Copyright (C) 2006-2009 Kenneth R. Sewell III
 
  This file is part of meshLib.
 
@@ -36,9 +36,65 @@ namespace ml
     cell(){}
     ~cell(){}
     
+    void setUnknown1( unsigned char u1 )
+    {
+      m_Unknown1 = u1;
+    }
+
+    unsigned char getUnknown1() const
+    {
+      return m_Unknown1;
+    }
+
+    void setName( const std::string &name )
+    {
+      m_Name = name;
+    }
+    
+    const std::string &getName() const
+    {
+      return m_Name;
+    }
+
+    void setModelFilename( const std::string &filename )
+    {
+      m_ModelFilename = filename;
+    }
+    
+    const std::string &getModelFilename() const
+    {
+      return m_ModelFilename;
+    }
+
+    void setHasFloor( bool floor )
+    {
+      m_HasFloor = floor;
+    }
+
+    bool hasFloor() const
+    {
+      return m_HasFloor;
+    }
+
+    void setFloorFilename( const std::string &filename )
+    {
+      m_FloorFilename = filename;
+    }
+    
+    const std::string &getFloorFilename() const
+    {
+      return m_FloorFilename;
+    }
+
   protected:
     unsigned int numPortals;
     std::vector<portal> portals;
+
+    unsigned char m_Unknown1;
+    std::string m_Name;
+    std::string m_ModelFilename;
+    bool m_HasFloor;
+    std::string m_FloorFilename;
 
   private:
   };
