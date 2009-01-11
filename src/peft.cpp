472aeeb045910d5e6b0dc98f2c715eb574764786
Kenneth R. Sewell III <sewellkr3@gmail.com>
2009-01-11 05:46:40 +0000

    Use new base::read()
    git-svn-id: file:///home/svn/meshLib@139 a422a631-36e5-491f-99c6-f798677b70e4

--- src/peft.cpp
+++ src/peft.cpp
@@ -4,7 +4,7 @@
  *  \author Kenneth R. Sewell III
 
  meshLib is used for the parsing and exporting .msh models.
- Copyright (C) 2006,2007 Kenneth R. Sewell III
+ Copyright (C) 2006-2009 Kenneth R. Sewell III
 
  This file is part of meshLib.
 
@@ -41,23 +41,15 @@ peft::~peft()
 unsigned int peft::readPEFT( std::istream &file, std::string path )
 {
   basePath = path;
-  unsigned int total = 0;
-  std::string form;
   unsigned int peftSize;
-  std::string type;
-
-  total += readFormHeader( file, form, peftSize, type );
+  unsigned int total = readFormHeader( file, "PEFT", peftSize );
   peftSize += 8;
-  if( form != "FORM" || type != "PEFT" )
-    {
-      std::cout << "Expected Form of type PEFT: " << type << std::endl;
-      exit( 0 );
-    }
   std::cout << "Found PEFT form"
 	    << ": " << peftSize-12 << " bytes"
 	    << std::endl;
 
   unsigned int size;
+  std::string form, type;
   total += readFormHeader( file, form, size, type );
   if( form != "FORM" )
     {
@@ -93,24 +85,14 @@ unsigned int peft::readPEFT( std::istream &file, std::string path )
 
 unsigned int peft::readEMGP( std::istream &file )
 {
-    unsigned int total = 0;
-
-    std::string form;
     unsigned int emgpSize;
-    std::string type;
-
-    total += readFormHeader( file, form, emgpSize, type );
+    unsigned int total = readFormHeader( file, "EMGP", emgpSize );
     emgpSize += 8;
-    if( form != "FORM" || type != "EMGP" )
-    {
-	std::cout << "Expected Form of type EMGP: " << type << std::endl;
-	exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << ": " << emgpSize-12 << " bytes"
+    std::cout << "Found FORM EMGP: " << emgpSize-12 << " bytes"
 	      << std::endl;
 
     unsigned int size;
+    std::string form, type;
     total += readFormHeader( file, form, size, type );
     if( form != "FORM" )
       {
@@ -145,24 +127,14 @@ unsigned int peft::readEMGP( std::istream &file )
 
 unsigned int peft::readEMTR( std::istream &file )
 {
-    unsigned int total = 0;
-
-    std::string form;
     unsigned int emtrSize;
-    std::string type;
-
-    total += readFormHeader( file, form, emtrSize, type );
+    unsigned int total = readFormHeader( file, "EMTR", emtrSize );
     emtrSize += 8;
-    if( form != "FORM" || type != "EMTR" )
-    {
-	std::cout << "Expected Form of type EMTR: " << type << std::endl;
-	exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << ": " << emtrSize-12 << " bytes"
+    std::cout << "Found FORM EMTR: " << emtrSize-12 << " bytes"
 	      << std::endl;
 
     unsigned int size;
+    std::string form, type;
     total += readFormHeader( file, form, size, type );
     if( form != "FORM" )
       {
@@ -175,13 +147,7 @@ unsigned int peft::readEMTR( std::istream &file )
 
     total += readPTIM( file );
 
-#if 1
     total += readUnknown( file, emtrSize - total );
-#else
-    // Skip for now...Fix later
-    file.seekg( emtrSize - total, std::ios_base::cur );
-    total = emtrSize;
-#endif
 
     if( emtrSize == total )
     {
@@ -199,24 +165,14 @@ unsigned int peft::readEMTR( std::istream &file )
 
 unsigned int peft::readPTIM( std::istream &file )
 {
-    unsigned int total = 0;
-
-    std::string form;
     unsigned int ptimSize;
-    std::string type;
-
-    total += readFormHeader( file, form, ptimSize, type );
+    unsigned int total = readFormHeader( file, "PTIM", ptimSize );
     ptimSize += 8;
-    if( form != "FORM" || type != "PTIM" )
-    {
-	std::cout << "Expected Form of type PTIM: " << type << std::endl;
-	exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << ": " << ptimSize-12 << " bytes"
+    std::cout << "Found FORM PTIM: " << ptimSize-12 << " bytes"
 	      << std::endl;
 
     unsigned int size;
+    std::string type;
     total += readRecordHeader( file, type, size );
     if( type != "0001" )
     {
@@ -230,8 +186,7 @@ unsigned int peft::readPTIM( std::istream &file )
     unsigned int x;
     for( unsigned int i = 0; i < 6; ++i )
       {
-	file.read( (char *)&x, sizeof( x ) );
-	total += sizeof( x );
+	total += base::read( file, x );
 	std::cout << x << std::endl;
       }
 
@@ -251,13 +206,9 @@ unsigned int peft::readPTIM( std::istream &file )
 
 unsigned int peft::read0000( std::istream &file, unsigned int &num )
 {
-    unsigned int total = 0;
-
-    std::string form;
     unsigned int size;
     std::string type;
-
-    total += readRecordHeader( file, type, size );
+    unsigned int total = readRecordHeader( file, type, size );
     size += 8;
     if( type != "0000" )
     {
@@ -268,8 +219,7 @@ unsigned int peft::read0000( std::istream &file, unsigned int &num )
 	      << ": " << size << " bytes" 
 	      << std::endl;
 
-    file.read((char *)&num, sizeof( num ) );
-    total += sizeof( num );
+    total += base::read( file, num );
 
     if( size == total )
     {
@@ -286,552 +236,3 @@ unsigned int peft::read0000( std::istream &file, unsigned int &num )
 }
 
 
-#if 0
-unsigned int peft::readIMPL( std::istream &file )
-{
-    unsigned int total = 0;
-
-    std::string form;
-    unsigned int implSize;
-    std::string type;
-
-    total += readFormHeader( file, form, implSize, type );
-    implSize += 8;
-    if( form != "FORM" || type != "IMPL" )
-    {
-	std::cout << "Expected Form of type IMPL: " << type << std::endl;
-	exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << ": " << implSize-12 << " bytes"
-	      << std::endl;
-
-    unsigned int size;
-    total += readFormHeader( file, form, size, type );
-    if( form != "FORM")
-    {
-	std::cout << "Expected FORM not: " << form << std::endl;
-	exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << ": " << size-4 << " bytes"
-	      << std::endl;
-
-    unsigned int position;
-    while( total < implSize )
-    {
-	// Peek at next record, but keep file at same place.
-        position = file.tellg();
-        readFormHeader( file, form, size, type );
-        file.seekg( position, std::ios_base::beg );
-
-	if( form == "SCAP" )
-	{
-	    total += readSCAP( file );
-	}
-	else if( form == "OPTN" )
-	{
-	    total += readOPTN( file );
-	}
-	else if( form == "DATA" )
-	{
-	    total += readIMPLDATA( file );
-	}
-	else if( form == "FORM" && type == "PASS" )
-	{
-	    total += readPASS( file );
-	}
-	else
-	{
-	    std::cout << "Unexpected record: " << form << std::endl;
-	    exit( 0 );
-	}
-
-    }
-    if( implSize == total )
-    {
-	std::cout << "Finished reading IMPL" << std::endl;
-    }
-    else
-    {
-	std::cout << "FAILED in reading IMPL" << std::endl;
-	std::cout << "Read " << total << " out of " << implSize
-                  << std::endl;
-    }
-
-    return total;
-}
-
-
-unsigned int peft::readSCAP( std::istream &file )
-{
-    unsigned int total = 0;
-
-    std::string form;
-    unsigned int scapSize;
-    std::string type;
-
-    total += readRecordHeader( file, type, scapSize );
-    if( type != "SCAP" )
-    {
-        std::cout << "Expected record of type SCAP: " << type << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found record " << type 
-	      << " (Set Capabilities?) "
-	      << scapSize << " bytes"
-	      << std::endl;
-
-#if 1
-    unsigned int numScap = scapSize/4;
-
-    unsigned int data;
-    for( unsigned int i = 0; i < numScap; ++i )
-    {
-        file.read( (char*)&data, sizeof( data ) );
-	std::cout << std::bitset<32>( data ) << " " << std::endl;
-    }
-    total += scapSize;
-#else
-    total += readUnknown( file, scapSize );
-#endif
-    if( scapSize == (total-8) )
-    {
-	std::cout << "Finished reading SCAP" << std::endl;
-    }
-    else
-    {
-	std::cout << "FAILED in reading SCAP" << std::endl;
-	std::cout << "Read " << total << " out of " << scapSize
-                  << std::endl;
-    }
-
-    return total;
-}
-
-unsigned int peft::readOPTN( std::istream &file )
-{
-    unsigned int total = 0;
-
-    std::string form;
-    unsigned int optnSize;
-    std::string type;
-
-    total += readRecordHeader( file, type, optnSize );
-    if( type != "OPTN" )
-    {
-        std::cout << "Expected record of type OPTN: " << type << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found record " << type
-	      << ": " << optnSize << " bytes"
-	      << std::endl;
-
-    // Skip data for now
-    total += readUnknown( file, optnSize );
-
-    if( optnSize == (total-8) )
-    {
-	std::cout << "Finished reading OPTN" << std::endl;
-    }
-    else
-    {
-	std::cout << "FAILED in reading OPTN" << std::endl;
-	std::cout << "Read " << total << " out of " << optnSize
-                  << std::endl;
-    }
-
-    return total;
-}
-
-unsigned int peft::readIMPLDATA( std::istream &file )
-{
-    unsigned int total = 0;
-
-    std::string form;
-    unsigned int impldataSize;
-    std::string type;
-
-    total += readRecordHeader( file, type, impldataSize );
-    if( type != "DATA" )
-    {
-        std::cout << "Expected record of type DATA: " << type << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found record " << type
-	      << ": " << impldataSize << " bytes" 
-	      << std::endl;
-
-    // Skip data for now
-    total += readUnknown( file, impldataSize );
-
-    if( impldataSize == (total-8) )
-    {
-	std::cout << "Finished reading DATA" << std::endl;
-    }
-    else
-    {
-	std::cout << "FAILED in reading DATA" << std::endl;
-	std::cout << "Read " << total << " out of " << impldataSize
-                  << std::endl;
-    }
-
-    return total;
-}
-
-unsigned int peft::readPASS( std::istream &file )
-{
-    unsigned int total = 0;
-
-    std::string form;
-    unsigned int passSize;
-    std::string type;
-
-    total += readFormHeader( file, form, passSize, type );
-    passSize += 8;
-    if( form != "FORM" || type != "PASS" )
-    {
-        std::cout << "Expected FORM of type PASS: " << type << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << ": " << passSize-12 << " bytes"
-	      << std::endl;
-
-    unsigned int size;
-    total += readFormHeader( file, form, size, type );
-    if( form != "FORM" )
-    {
-        std::cout << "Expected FORM not: " << form << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << ": " << size-4 << " bytes"
-	      << std::endl;
-
-    total += readRecordHeader( file, type, size );
-    if( type != "DATA" )
-    {
-        std::cout << "Expected record of type DATA: " << type << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found record " << type
-	      << ": " << size << " bytes" 
-	      << std::endl;
-
-    total += readUnknown( file, size );
-
-    // Peek at next record, but keep file at same place.
-    unsigned int position = file.tellg();
-    readFormHeader( file, form, size, type );
-    file.seekg( position, std::ios_base::beg );
-
-    if( form == "FORM" && type == "PVSH" )
-    {
-	total += readPVSH( file );
-	total += readPPSH( file );
-    }
-    else if( form == "FORM" && type == "PFFP" )
-    {
-	total += readPFFP( file );
-	total += readSTAG( file );
-	if( total < passSize )
-	{
-	    total += readSTAG( file );
-	}
-    }
-    else
-    {
-	std::cout << "Unexpected FORM: " << type << std::endl;
-	exit( 0 );
-    }
-
-    if( passSize == total )
-    {
-	std::cout << "Finished reading PASS" << std::endl;
-    }
-    else
-    {
-	std::cout << "FAILED in reading PASS" << std::endl;
-	std::cout << "Read " << total << " out of " << passSize
-                  << std::endl;
-    }
-
-    return total;
-}
-
-unsigned int peft::readPVSH( std::istream &file )
-{
-    unsigned int total = 0;
-
-    std::string form;
-    unsigned int pvshSize;
-    std::string type;
-
-    total += readFormHeader( file, form, pvshSize, type );
-    pvshSize += 8;
-    if( form != "FORM" || type != "PVSH" )
-    {
-	std::cout << "Expected Form of type PVSH: " << type << std::endl;
-	exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << " (Vertex shader)"
-	      << ": " << pvshSize-12 << " bytes"
-	      << std::endl;
-
-    unsigned int size;
-    total += readRecordHeader( file, type, size );
-    if( type != "0000" )
-    {
-        std::cout << "Expected record of type 0000: " << type << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found record " << type
-	      << ": " << size << " bytes"
-	      << std::endl;
-
-    char temp[255];
-    std::string vertexProgramName;
-    file.getline( temp, size, 0 );
-    total += size;
-    vertexProgramName = temp;
-
-    std::cout << "Vertex program: " << vertexProgramName << std::endl;
-
-
-    if( pvshSize == total )
-    {
-	std::cout << "Finished reading PVSH" << std::endl;
-    }
-    else
-    {
-	std::cout << "FAILED in reading PVSH" << std::endl;
-	std::cout << "Read " << total << " out of " << pvshSize
-                  << std::endl;
-    }
-
-    return total;
-}
-
-unsigned int peft::readPFFP( std::istream &file )
-{
-    unsigned int total = 0;
-
-    std::string form;
-    unsigned int pffpSize;
-    std::string type;
-
-    total += readFormHeader( file, form, pffpSize, type );
-    pffpSize += 8;
-    if( form != "FORM" || type != "PFFP" )
-    {
-	std::cout << "Expected Form of type PFFP: " << type << std::endl;
-	exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type 
-	      << " (Fixed Function Pipeline)"
-	      << ": " << pffpSize-12 << " bytes"
-	      << std::endl;
-
-    unsigned int size;
-    total += readRecordHeader( file, type, size );
-    if( type != "0001" )
-    {
-        std::cout << "Expected record of type 0001: " << type << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found record " << type
-	      << ": " << size << " bytes"
-	      << std::endl;
-
-    total += readUnknown( file, size );
-
-    if( pffpSize == total )
-    {
-	std::cout << "Finished reading PFFP" << std::endl;
-    }
-    else
-    {
-	std::cout << "FAILED in reading PFFP" << std::endl;
-	std::cout << "Read " << total << " out of " << pffpSize
-                  << std::endl;
-    }
-
-    return total;
-}
-
-unsigned int peft::readSTAG( std::istream &file )
-{
-    unsigned int total = 0;
-
-    std::string form;
-    unsigned int stagSize;
-    std::string type;
-
-    total += readFormHeader( file, form, stagSize, type );
-    stagSize += 8;
-    if( form != "FORM" || type != "STAG" )
-    {
-	std::cout << "Expected Form of type STAG: " << type << std::endl;
-	exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << ": " << stagSize-12 << " bytes"
-	      << std::endl;
-
-    unsigned int size;
-    total += readRecordHeader( file, type, size );
-    if( type != "0000" )
-    {
-        std::cout << "Expected record of type 0000: " << type << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found record " << type
-	      << ": " << size << " bytes"
-	      << std::endl;
-
-    // Skip data for now
-    total += readUnknown( file, size );
-
-    if( stagSize == total )
-    {
-	std::cout << "Finished reading STAG" << std::endl;
-    }
-    else
-    {
-	std::cout << "FAILED in reading STAG" << std::endl;
-	std::cout << "Read " << total << " out of " << stagSize
-                  << std::endl;
-    }
-
-    return total;
-}
-
-unsigned int peft::readPPSH( std::istream &file )
-{
-    unsigned int total = 0;
-
-    std::string form;
-    unsigned int ppshSize;
-    std::string type;
-
-    total += readFormHeader( file, form, ppshSize, type );
-    ppshSize += 8;
-    if( form != "FORM" || type != "PPSH" )
-    {
-	std::cout << "Expected Form of type PPSH: " << type << std::endl;
-	exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type 
-	      << " (Pixel shader)"
-	      << ": " << ppshSize-12 << " bytes"
-	      << std::endl;
-
-    unsigned int size;
-    total += readFormHeader( file, form, size, type );
-    if( form != "FORM" )
-    {
-        std::cout << "Expected FORM not: " << form << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << ": " << size-4 << " bytes"
-	      << std::endl;
-
-    total += readRecordHeader( file, type, size );
-    if( type != "DATA" )
-    {
-        std::cout << "Expected record of type DATA: " << type << std::endl;
-        exit( 0 );
-    }
-    std::cout << "Found record " << type
-	      << ": " << size << " bytes" 
-	      << std::endl;
-
-    std::string pixelShaderName;
-    unsigned char num;
-    file.read( (char *)&num, sizeof( unsigned char ) );
-    char temp[255];
-    file.getline( temp, size - sizeof( unsigned char ), 0 );
-    pixelShaderName = temp;
-    std::cout << "Number of texture arguments to pixel shader: "
-	      << (unsigned int)num << std::endl;
-    std::cout << "Pixel shader: " << pixelShaderName << std::endl;;
-    total += size;
-
-    while( total < ppshSize )
-    {
-	total += readPTXM( file );
-    }
-
-    if( ppshSize == total )
-    {
-	std::cout << "Finished reading PPSH" << std::endl;
-    }
-    else
-    {
-	std::cout << "FAILED in reading PPSH" << std::endl;
-	std::cout << "Read " << total << " out of " << ppshSize
-                  << std::endl;
-    }
-
-    return total;
-}
-
-unsigned int peft::readPTXM( std::istream &file )
-{
-    unsigned int total = 0;
-
-    std::string form;
-    unsigned int ptxmSize;
-    std::string type;
-
-    total += readFormHeader( file, form, ptxmSize, type );
-    ptxmSize += 8;
-    if( form != "FORM" || type != "PTXM" )
-    {
-	std::cout << "Expected Form of type PTXM: " << type << std::endl;
-	exit( 0 );
-    }
-    std::cout << "Found " << form << " " << type
-	      << ": " << ptxmSize-12 << " bytes"
-	      << std::endl;
-
-    unsigned int size;
-    total += readRecordHeader( file, type, size );
-#if 0
-    if( type != "0002" )
-    {
-        std::cout << "Expected record of type 0002: " << type << std::endl;
-        exit( 0 );
-    }
-#endif
-    std::cout << "Found record " << type
-	      << ": " << size << " bytes"
-	      << std::endl;
-
-    std::string name;
-    unsigned char num;
-    file.read( (char *)&num, sizeof( unsigned char ) );
-    file.width( size - sizeof( unsigned char ) );
-    file >> name;
-    total += size;
-    std::cout << "Pixel shader texture " << (unsigned int)num
-	      << ": " << name << std::endl;
-
-    if( ptxmSize == total )
-    {
-	std::cout << "Finished reading PTXM" << std::endl;
-    }
-    else
-    {
-	std::cout << "FAILED in reading PTXM" << std::endl;
-	std::cout << "Read " << total << " out of " << ptxmSize
-                  << std::endl;
-    }
-
-    return total;
-}
-
-#endif
