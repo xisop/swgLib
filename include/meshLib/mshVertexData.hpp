25a59060c420108ec18469b0dcd0332bbb9c730a
Kenneth R. Sewell III <sewellkr3@gmail.com>
2009-01-11 21:41:01 +0000

    Updated copyright
    git-svn-id: file:///home/svn/meshLib@154 a422a631-36e5-491f-99c6-f798677b70e4

--- include/meshLib/mshVertexData.hpp
+++ include/meshLib/mshVertexData.hpp
@@ -3,7 +3,7 @@
  *  \file   mshVertexData.hpp
  *  \author Kenneth R. Sewell III
 
- Copyright (C) 2006,2007 Kenneth R. Sewell III
+ Copyright (C) 2006-2009 Kenneth R. Sewell III
 
  This file is part of meshLib.
 
