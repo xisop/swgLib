22806faead1b047978a4b91af350efe4211b9fe2
Ken Sewell <xisop@users.noreply.github.com>
2021-10-25 15:05:48 -0400

    Handle scroll textures.  Handle inline effects.

--- include/swgLib/sht.hpp
+++ include/swgLib/sht.hpp
@@ -23,9 +23,10 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 #include <swgLib/base.hpp>
+#include <swgLib/color4.hpp>
+#include <swgLib/efct.hpp>
 #include <swgLib/matl.hpp>
 #include <swgLib/txm.hpp>
-#include <swgLib/color4.hpp>
 
 #include <istream>
 #include <string>
@@ -43,17 +44,29 @@ namespace ml
 		sht();
 		~sht();
 
-		std::size_t readSHT(std::istream& file, std::string path = "");
+		std::size_t readSHT(std::istream& file);
 
-	  const std::vector<matl> &getMaterials() const;
-	  const std::vector<txm> &getTextures() const;
-	  const std::string &getEffectName() const;
-	  uint8_t getTexCoordSet( const txm& tex ) const;
+		const std::vector<matl>& getMaterials() const;
+		const std::vector<txm>& getTextures() const;
+		const std::string& getEffectName() const;
+		uint8_t getTexCoordSet(const txm& tex) const;
+
+	public:
+		struct textureScroll {
+			float u1, v1;
+			float u2, v2;
+		};
+
+	protected:
+		std::size_t readV0(std::istream& file);
+		std::size_t readV1(std::istream& file);
+
 	protected:
 		std::size_t readMATS(std::istream& file);
 		std::size_t readTXMS(std::istream& file);
 		std::size_t readTCSS(std::istream& file);
 		std::size_t readTFNS(std::istream& file);
+		std::size_t readTSNS(std::istream& file);
 		std::size_t readARVS(std::istream& file);
 		std::size_t readSRVS(std::istream& file);
 
@@ -62,66 +75,16 @@ namespace ml
 		std::vector<matl> _material;
 		std::vector<txm>  _texture;
 
-		std::map<tag, uint8_t> _texCoordSet;
-		std::map<tag, color4>  _texFactor;
-		std::map<tag, uint8_t> _alphaReferenceValue;
-		std::map<tag, uint32_t> _stencilReferenceValue;
+		std::map<tag, uint8_t>       _texCoordSet;
+		std::map<tag, color4>        _texFactor;
+		std::map<tag, uint8_t>       _alphaReferenceValue;
+		std::map<tag, uint32_t>      _stencilReferenceValue;
+		std::map<tag, textureScroll> _texScroll;
 
 		std::string _effectName;
+		efct        _effect;
 
-
-
-
-		/*
-				unsigned int readTSNS(std::istream& file, const unsigned short& depth = 0);
-				unsigned int readNAME(std::istream& file, const unsigned short& depth = 0);
-				unsigned int readEFCT(std::istream& file, const unsigned short& depth = 0);
-
-				std::vector<std::string> texTag;
-
-				std::string materialName;
-
-				std::string diffuseTextureTag;
-				std::string diffuseTextureName;
-
-				std::string mainTextureName;
-				unsigned int mainTextureUnit;
-
-				std::string normalTextureName;
-				unsigned int normalTextureUnit;
-
-				std::string specularTextureName;
-				unsigned int specularTextureUnit;
-
-				std::string hueTextureName;
-				unsigned int hueTextureUnit;
-
-				std::string environmentTextureName;
-				unsigned int environmentTextureUnit;
-
-				std::string dot3TextureName;
-				unsigned int dot3TextureUnit;
-
-				std::string lookupTextureName;
-				unsigned int lookupTextureUnit;
-
-				std::string maskTextureName;
-				unsigned int maskTextureUnit;
-
-				float ambient[4];
-				float diffuse[4];
-				float specular[4];
-				float emissive[4];
-				float shininess;
-
-				bool normalMap;
-				std::string effectName;
-
-				unsigned int normalMapUnit;
-				unsigned int mainMapUnit;
-				*/
 	private:
-
 	};
 }
 #endif
--- src/swgLib/sht.cpp
+++ src/swgLib/sht.cpp
@@ -35,13 +35,13 @@ sht::~sht()
 {
 }
 
-const std::vector<matl> &sht::getMaterials() const { return _material;}
+const std::vector<matl>& sht::getMaterials() const { return _material; }
 
-const std::vector<txm> &sht::getTextures() const { return _texture;}
+const std::vector<txm>& sht::getTextures() const { return _texture; }
 
-const std::string &sht::getEffectName() const { return _effectName; }
+const std::string& sht::getEffectName() const { return _effectName; }
 
-uint8_t sht::getTexCoordSet( const txm& tex ) const {
+uint8_t sht::getTexCoordSet(const txm& tex) const {
 	const auto& set = _texCoordSet.find(tex.getNameTag());
 	if (_texCoordSet.end() == set) {
 		return 0;
@@ -51,7 +51,7 @@ uint8_t sht::getTexCoordSet( const txm& tex ) const {
 	}
 }
 
-std::size_t sht::readSHT(std::istream& file, std::string path)
+std::size_t sht::readSHT(std::istream& file)
 {
 	std::size_t sshtSize;
 	std::size_t total = base::readFormHeader(file, "SSHT", sshtSize);
@@ -59,22 +59,113 @@ std::size_t sht::readSHT(std::istream& file, std::string path)
 	std::cout << "Found SSHT form: " << sshtSize << " bytes\n";
 
 	std::size_t size;
-	std::string type;
-	total += base::readFormHeader(file, type, size);
-
+	std::string form, type;
+	base::peekHeader(file, form, size, type);
 	_version = base::tagToVersion(type);
-	if ((0 != _version) && (1 != _version)) {
-		std::cout << "Expected version 0000 or 0001. Found " << type << "\n";
+	std::cout << "SSHT version: " << (int)_version << "\n";
+
+	if (0 == _version) {
+		total += readV0(file);
+	}
+	else if (1 == _version) {
+		total += readV1(file);
+	}
+	else {
+		std::cout << "Unsupported Static Shader (SSHT) version: " << (int)_version << "\n";
 		exit(0);
 	}
-	std::cout << "Found form " << type << "\n";
-	std::cout << "SSHT version: " << (int)_version << "\n";
-	
-	std::string form;
 
-	if (1 == _version) {
-		// Load effect...
-		std::cout << "Not handled yet: " << __FILE__ << ": " << __LINE__ << "\n";
+	if (sshtSize == total) {
+		std::cout << "Finished reading SSHT\n";
+	}
+	else {
+		std::cout << "FAILED in reading SSHT\n";
+		std::cout << "Read " << total << " out of " << sshtSize << "\n";
+	}
+
+	return total;
+}
+
+std::size_t sht::readV0(std::istream& file) {
+	std::size_t size;
+	std::size_t total = base::readFormHeader(file, "0000", size);
+
+	std::string form, type;
+	// Load materials...
+	base::peekHeader(file, form, size, type);
+	if (("FORM" == form) && ("MATS" == type)) {
+		total += readMATS(file);
+	}
+
+	// Load textures...
+	base::peekHeader(file, form, size, type);
+	if (("FORM" == form) && ("TXMS" == type)) {
+		total += readTXMS(file);
+	}
+
+	// Load texture coordinate set...
+	base::peekHeader(file, form, size, type);
+	if (("FORM" == form) && ("TCSS" == type)) {
+		total += readTCSS(file);
+	}
+
+	// Load texture factor...
+	base::peekHeader(file, form, size, type);
+	if (("FORM" == form) && ("TFNS" == type)) {
+		total += readTFNS(file);
+	}
+
+	// Load alpha reverence value...
+	base::peekHeader(file, form, size, type);
+	if (("FORM" == form) && ("ARVS" == type)) {
+		total += readARVS(file);
+	}
+
+	// Load stencil reference value...
+	base::peekHeader(file, form, size, type);
+	if (("FORM" == form) && ("SRVS" == type)) {
+		total += readSRVS(file);
+	}
+
+	// Load effect...
+	base::peekHeader(file, form, size, type);
+	if ("NAME" == type) {
+		total += base::readRecordHeader(file, "NAME", size);
+		std::cout << "Found record NAME: " << size << "\n";
+		total += base::read(file, _effectName);
+		base::fixSlash(_effectName);
+		std::cout << "Effect: " << _effectName << "\n";
+	}
+	else if ("EFCT" == type) {
+		total += _effect.readEFCT(file);
+	}
+	else {
+		std::cout << "Unhandled effect type: " << type << "\n";
+		exit(0);
+	}
+
+	return total;
+}
+
+std::size_t sht::readV1(std::istream& file) {
+	std::size_t size;
+	std::size_t total = base::readFormHeader(file, "0001", size);
+
+	// Load effect...
+	std::string form, type;
+	base::peekHeader(file, form, size, type);
+	if ("NAME" == type) {
+		total += base::readRecordHeader(file, "NAME", size);
+		std::cout << "Found record NAME: " << size << "\n";
+		total += base::read(file, _effectName);
+		base::fixSlash(_effectName);
+		std::cout << "Effect: " << _effectName << "\n";
+	}
+	else if ("EFCT" == type) {
+		total += _effect.readEFCT(file);
+	}
+	else {
+		std::cout << "Unhandled effect type: " << type << "\n";
 		exit(0);
 	}
 
@@ -102,10 +193,10 @@ std::size_t sht::readSHT(std::istream& file, std::string path)
 		total += readTFNS(file);
 	}
 
-	if (1 == _version) {
-		// Load texture scroll...
-		std::cout << "Not handled yet: " << __FILE__ << ": " << __LINE__ << "\n";
-		exit(0);
+	// Load TSNS...
+	base::peekHeader(file, form, size, type);
+	if (("FORM" == form) && ("TSNS" == type)) {
+		total += readTSNS(file);
 	}
 
 	// Load alpha reverence value...
@@ -118,25 +209,6 @@ std::size_t sht::readSHT(std::istream& file, std::string path)
 	base::peekHeader(file, form, size, type);
 	if (("FORM" == form) && ("SRVS" == type)) {
 		total += readSRVS(file);
-	}
-
-	if (0 == _version) {
-		// Load effect...
-		total += base::readRecordHeader(file, "NAME", size);
-		std::cout << "Found record NAME: " << size << "\n";
-		total += base::read(file, _effectName);
-		base::fixSlash(_effectName);
-		std::cout << "Effect: " << _effectName << "\n";
-	}
-
-	if (sshtSize == total)
-	{
-		std::cout << "Finished reading SSHT\n";
-	}
-	else
-	{
-		std::cout << "FAILED in reading SSHT\n";
-		std::cout << "Read " << total << " out of " << sshtSize << "\n";
 	}
 
 	return total;
@@ -198,17 +270,15 @@ std::size_t sht::readTXMS(std::istream& file)
 	std::size_t txmsSize;
 	std::size_t total = base::readFormHeader(file, "TXMS", txmsSize);
 	txmsSize += 8;
-	std::size_t dataSize = txmsSize - 12;
-	std::cout << "Found FORM TXMS: " << dataSize << " bytes\n";
+	std::cout << "Found FORM TXMS: " << txmsSize << " bytes\n";
 
-	while (dataSize > 0) {
+	while (total < txmsSize ) {
 		std::cout << "Reading texture " << _texture.size() << ":\n";
 		_texture.resize(_texture.size() + 1);
 
 		// Read texture...
 		const std::size_t txmSize = _texture.back().read(file);
 
-		dataSize -= txmSize;
 		total += txmSize;
 	}
 
@@ -247,121 +317,10 @@ std::size_t sht::readTCSS(std::istream& file)
 		std::cout << "Tex Coord Set: " << texTag << ": " << (int)index << "\n";
 	}
 
-	/*
-			if (texName == "NIAM")
-			{
-				mainTextureUnit = texUnit;
-			}
-			else if (texName == "LMRN")
-			{
-				normalTextureUnit = texUnit;
-			}
-			else if (texName == "3TOD")
-			{
-				dot3TextureUnit = texUnit;
-			}
-			else if (texName == "PUKL")
-			{
-				lookupTextureUnit = texUnit;
-			}
-			else if (texName == "MVNE")
-			{
-				environmentTextureUnit = texUnit;
-			}
-			else if (texName == "KSAM")
-			{
-				maskTextureUnit = texUnit;
-			}
-			else if (texName == "BEUH")
-			{
-				hueTextureUnit = texUnit;
-			}
-			else if (texName == "CEPS")
-			{
-				specularTextureUnit = texUnit;
-			}
-			else if (texName == "MRNC")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "SIME")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "1PLA")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "2PLA")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "3PLA")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "ATED")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "ALTD")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "BLTD")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "TRID")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "DIRI")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "NRCS")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "0PER")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "LACD")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "YKS_")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "EBUC")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "PFFM")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else if (texName == "NEPO")
-			{
-				//specularTextureUnit = texUnit;
-			}
-			else
-			{
-				std::cout << "Unknown texture tag: " << texName << std::endl;
-				exit(0);
-			}
-		}
-		*/
-
-	if (tcssSize == total)
-	{
+	if (tcssSize == total) {
 		std::cout << "Finished reading TCSS\n";
 	}
-	else
-	{
+	else {
 		std::cout << "FAILED in reading TCSS\n";
 		std::cout << "Read " << total << " out of " << tcssSize << "\n";
 		exit(0);
@@ -382,7 +341,7 @@ std::size_t sht::readTFNS(std::istream& file)
 	std::cout << "Found record 0000: " << size << " bytes\n";
 
 	const uint32_t num = uint32_t(size / 8);
-	for(uint32_t i=0; i < num; ++i ) {
+	for (uint32_t i = 0; i < num; ++i) {
 		tag texTag;
 		total += base::read(file, texTag);
 
@@ -401,6 +360,44 @@ std::size_t sht::readTFNS(std::istream& file)
 	{
 		std::cout << "FAILED in reading TFNS\n";
 		std::cout << "Read " << total << " out of " << tfnsSize << "\n";
+		exit(0);
+	}
+
+	return total;
+}
+
+std::size_t sht::readTSNS(std::istream& file) {
+	std::size_t tsnsSize;
+	std::size_t total = base::readFormHeader(file, "TSNS", tsnsSize);
+	tsnsSize += 8;
+	std::cout << "Found FORM TSNS: " << tsnsSize - 12 << " bytes\n";
+
+	std::size_t size;
+	total += base::readRecordHeader(file, "0000", size);
+	std::cout << "Found record 0000: " << size << " bytes\n";
+
+	tag texTag;
+	total += base::read(file, texTag);
+	textureScroll scroll;
+	total += base::read(file, scroll.u1);
+	total += base::read(file, scroll.v1);
+	total += base::read(file, scroll.u2);
+	total += base::read(file, scroll.v2);
+
+	std::cout << "Scroll texture: " << texTag.str() << "\n"
+		<< "u1: " << scroll.u1 << "\n"
+		<< "v1: " << scroll.v1 << "\n"
+		<< "u2: " << scroll.u2 << "\n"
+		<< "v2: " << scroll.v2 << "\n";
+
+	_texScroll[texTag] = scroll;
+
+	if (tsnsSize == total) {
+		std::cout << "Finished reading TSNS\n";
+	}
+	else {
+		std::cout << "FAILED in reading TSNS\n";
+		std::cout << "Read " << total << " out of " << tsnsSize << "\n";
 		exit(0);
 	}
 
@@ -478,129 +475,3 @@ std::size_t sht::readSRVS(std::istream& file)
 
 	return total;
 }
-
-/*
-unsigned int sht::readTSNS(std::istream& file, const unsigned short& depth)
-{
-	std::size_t tsnsSize;
-	std::size_t total = readFormHeader(file, "TSNS", tsnsSize);
-	tsnsSize += 8;
-	std::cout << std::string(depth, ' ')
-		<< "Found FORM TSNS: " << tsnsSize - 12 << " bytes"
-		<< std::endl;
-
-	std::size_t size;
-	std::string type;
-	total += readRecordHeader(file, type, size);
-	if (type != "0000")
-	{
-		std::cout << "Expected record of type 0000: " << type << std::endl;
-		exit(0);
-	}
-	std::cout << std::string(depth, ' ')
-		<< "Found record " << type
-		<< ": " << size << " bytes"
-		<< std::endl;
-
-	total += readUnknown(file, size);
-
-	if (tsnsSize == total)
-	{
-		std::cout << std::string(depth, ' ')
-			<< "Finished reading TSNS" << std::endl;
-	}
-	else
-	{
-		std::cout << "FAILED in reading TSNS" << std::endl;
-		std::cout << "Read " << total << " out of " << tsnsSize
-			<< std::endl;
-	}
-
-	return total;
-}
-
-
-unsigned int sht::readEFCT(std::istream& file, const unsigned short& depth)
-{
-	std::size_t efctSize;
-	std::string type;
-
-	std::size_t total = readFormHeader(file, "EFCT", efctSize);
-	efctSize += 8;
-	std::cout << std::string(depth, ' ')
-		<< "Found FORM EFCT: " << efctSize - 12 << " bytes"
-		<< std::endl;
-
-	std::size_t size;
-	total += readFormHeader(file, "0001", size);
-	std::cout << std::string(depth, ' ')
-		<< "Found FORM 0001: " << size - 4 << " bytes"
-		<< std::endl;
-
-	total += readRecordHeader(file, type, size);
-	if (type != "DATA")
-	{
-		std::cout << "Expected record of type DATA: " << type << std::endl;
-		exit(0);
-	}
-	std::cout << std::string(depth, ' ')
-		<< "Found record " << type
-		<< ": " << size << " bytes"
-		<< std::endl;
-
-	total += readUnknown(file, size);
-
-	if (efctSize == total)
-	{
-		std::cout << std::string(depth, ' ')
-			<< "Finished reading EFCT" << std::endl;
-	}
-	else
-	{
-		std::cout << "FAILED in reading EFCT" << std::endl;
-		std::cout << "Read " << total << " out of " << efctSize
-			<< std::endl;
-	}
-
-	return total;
-}
-
-unsigned int sht::readNAME(std::istream& file, const unsigned short& depth)
-{
-	std::size_t nameSize;
-	std::string type;
-	// Read Effect file record
-	std::size_t total = readRecordHeader(file, type, nameSize);
-	nameSize += 8;
-	if (type != "NAME")
-	{
-		std::cout << "Expected record of type NAME: " << type << std::endl;
-		return 0;
-	}
-	std::cout << std::string(depth, ' ')
-		<< "Found record " << type
-		<< ": " << nameSize << " bytes"
-		<< std::endl;
-
-	total += base::read(file, effectName);
-	base::fixSlash(effectName);
-
-	std::string fullEffectName = basePath + effectName;
-	std::cout << std::string(depth + 1, ' ')
-		<< "Effect file: " << fullEffectName << std::endl;
-
-	if (nameSize == total)
-	{
-		std::cout << std::string(depth, ' ')
-			<< "Finished reading NAME" << std::endl;
-	}
-	else
-	{
-		std::cout << "FAILED in reading NAME" << std::endl;
-		std::cout << "Read " << total << " out of " << nameSize
-			<< std::endl;
-	}
-
-	return total;
-}
-*/
